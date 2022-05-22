60fd18c10559ae33592bc5db7469b7ebfb85911b
firatkaanbitmez <74864221+firatkaanbitmez@users.noreply.github.com>
2022-05-22 23:43:45 +0300

    Add files via upload

---
+++ How to Remove 'Show More Options' From the Windows 11 Context Menu.bat
@@ -0,0 +1,18 @@
+
+@echo off
+title C.By.ARTHHH
+color 0A
+cls
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+
+
+cls 
+
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+taskkill /f /im explorer.exe
+explorer.exe
+
+
+
+exit
