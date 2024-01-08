2fd55b4f1a3d120d0df133e3c62d54eb55737602
FIRAT KAAN BİTMEZ <74864221+firatkaanbitmez@users.noreply.github.com>
2024-01-08 14:01:10 +0300

    Update and rename Disable_Windows_Copilot.bat to Disable_Copilot_and_DevHome.bat

--- Disable_Copilot_and_DevHome.bat
+++ Disable_Copilot_and_DevHome.bat
@@ -3,4 +3,6 @@
 :: Disable Windows Copilot in Windows 11
 REG ADD "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsCopilot" /v TurnOffWindowsCopilot /t REG_DWORD /d 1 /f
 echo Windows Copilot has been disabled.
+powershell -command "Get-AppxPackage *Microsoft.Windows.devhome* | Remove-AppxPackage"
+echo Windows devHome has been uninstall.
 pause
